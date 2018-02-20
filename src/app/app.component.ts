649c5f67cac1005a002975351e6051f23777ccdc
AMIRAULT Tony <tony.amirault@allianz.fr>
2018-02-20 10:38:16 +0100

    allow update when present

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private swUpdate: SwUpdate) {
+    this.swUpdate.available.subscribe((event) => {
+      this.swUpdate.activateUpdate().then(() => {
+        document.location.reload();
+      });
+    });
+  }
 }
